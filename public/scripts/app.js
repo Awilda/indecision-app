1925ef396c434751ace2c5d333553f5bf25dcc28
Awilda I Couverthier <awildacouverthier@gmail.com>
2019-10-11 21:29:38 -0400

    picking an option

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -25,6 +25,12 @@ var removeAll = function removeAll() {
 	renderOptions();
 };
 
+var onMakeDecision = function onMakeDecision() {
+	var randomNum = Math.floor(Math.random() * app.options.length);
+	var option = app.options[randomNum];
+	alert(option);
+};
+
 var appRoot = document.getElementById('app');
 
 var renderOptions = function renderOptions() {
@@ -47,9 +53,9 @@ var renderOptions = function renderOptions() {
 			app.options.length > 0 ? 'Here are your options: ' : 'No options'
 		),
 		React.createElement(
-			'p',
-			null,
-			app.options.length
+			'button',
+			{ disabled: app.options.length === 0, onClick: onMakeDecision },
+			'What should I do?'
 		),
 		React.createElement(
 			'button',
--- src/app.js
+++ src/app.js
@@ -23,6 +23,12 @@ const removeAll = () => {
 	renderOptions();
 };
 
+const onMakeDecision = () => {
+	const randomNum = Math.floor(Math.random() * app.options.length);
+	const option = app.options[randomNum];
+	alert(option);
+};
+
 const appRoot = document.getElementById('app');
 
 const renderOptions = () => {
@@ -32,7 +38,7 @@ const renderOptions = () => {
 			{app.subtitle && <p>{app.subtitle}</p>}
 			<p>{app.options.length > 0 ? 'Here are your options: ' : 'No options'}
 			</p>
-			<p>{app.options.length}</p>
+			<button disabled={app.options.length === 0} onClick={onMakeDecision}>What should I do?</button>
 			<button onClick={removeAll}>Remove All</button>
 			<ol>
 				{
