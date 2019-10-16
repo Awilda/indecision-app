8cbb4776a93bebd9c412caf79dfe371f102ee857
Awilda I Couverthier <awildacouverthier@gmail.com>
2019-10-16 09:22:52 -0400

    method binding

--- src/app.js
+++ src/app.js
@@ -42,8 +42,12 @@ class Action extends React.Component {
 }
 
 class Options extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleRemoveAll = this.handleRemoveAll.bind(this);
+	}
 	handleRemoveAll() {
-		alert('handleRemoveAll');
+		console.log(this.props.options);
 	}
 	render() {
 		return (
