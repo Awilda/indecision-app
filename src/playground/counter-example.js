03927bb9486e75d821903028e8bb8c1635721339
Awilda I Couverthier <awildacouverthier@gmail.com>
2019-10-17 13:34:16 -0400

    add state to counter app pt 1

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -1,28 +1,58 @@
-let count = 0;
-const addOne = () => {
-	count++;
-	renderCounterApp();
-};
-const minusOne = () => {
-	count--;
-	renderCounterApp();
-};
-const reset = () => {
-	count = 0;
-	renderCounterApp();
-};
+class Counter extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleAddOne = this.handleAddOne.bind(this);
+		this.handleMinusOne = this.handleMinusOne.bind(this);
+		this.handleReset = this.handleReset.bind(this);
+	}
+	handleAddOne() {
+		console.log('handleAddOne');
+	}
+	handleMinusOne() {
+		console.log('handleMinusOne');
+	}
+	handleReset() {
+		console.log('handleReset');		
+	}
+	render() {
+		return (
+			<div>
+				<h1>Count: </h1>
+				<button onClick={this.handleAddOne}>+1</button>
+				<button onClick={this.handleMinusOne}>-1</button>
+				<button onClick={this.handleReset}>reset</button>
+			</div>
+		);
+	}
+}
 
-const renderCounterApp = () => {
-	const templateTwo = (
-	<div>
-		<h1>Count: {count}</h1>
-		<button onClick={addOne}>+1</button>
-		<button onClick={minusOne}>-1</button>
-		<button onClick={reset}>reset</button>
-	</div>
-);
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
-	ReactDOM.render(templateTwo, appRoot);
-};
+// let count = 0;
+// const addOne = () => {
+// 	count++;
+// 	renderCounterApp();
+// };
+// const minusOne = () => {
+// 	count--;
+// 	renderCounterApp();
+// };
+// const reset = () => {
+// 	count = 0;
+// 	renderCounterApp();
+// };
 
-renderCounterApp();
+// const renderCounterApp = () => {
+// 	const templateTwo = (
+// 	<div>
+// 		<h1>Count: {count}</h1>
+// 		<button onClick={addOne}>+1</button>
+// 		<button onClick={minusOne}>-1</button>
+// 		<button onClick={reset}>reset</button>
+// 	</div>
+// );
+
+// 	ReactDOM.render(templateTwo, appRoot);
+// };
+
+// renderCounterApp();
