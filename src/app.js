b25347fdbacf0f7191b603e09a55c4548469dec0
Awilda I Couverthier <awildacouverthier@gmail.com>
2019-12-06 17:01:31 -0500

    default prop values

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ class IndecisionApp extends React.Component {
 		this.handlePick = this.handlePick.bind(this);
 		this.handleAddOption = this.handleAddOption.bind(this);
 		this.state = {
-			options: []
+			options: props.options
 		};
 	}
 	handleDeleteOptions() {
@@ -36,12 +36,11 @@ class IndecisionApp extends React.Component {
 		});
 	}
 	render() {
-		const title = 'Indecision';
 		const subtitle = 'Put your life in the hands of a computer';
 
 		return (
 			<div>
-				<Header title ={title} subtitle={subtitle} />
+				<Header subtitle={subtitle} />
 				<Action 
 					hasOptions={this.state.options.length > 0} 
 					handlePick={this.handlePick}
@@ -58,14 +57,22 @@ class IndecisionApp extends React.Component {
 	};
 }
 
+IndecisionApp.defaultProps = {
+	options: []
+}
+
 const Header = (props) => {
 	return (
 		<div>
 			<h1>{props.title}</h1>
-			<h2>{props.subtitle}</h2>
+			{props.subtitle && <h2>{props.subtitle}</h2>}
 		</div>
 	);
 }
+
+Header.defaultProps = {
+	title: 'Indecision'
+};
 
 const Action = (props) => {
 	return (
--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,7 +5,7 @@ class Counter extends React.Component {
 		this.handleMinusOne = this.handleMinusOne.bind(this);
 		this.handleReset = this.handleReset.bind(this);
 		this.state = {
-			count: 0
+			count: props.count
 		};
 	}
 	handleAddOne() {
@@ -27,7 +27,7 @@ class Counter extends React.Component {
 			return {
 				count: 0
 			}
-		});		
+		});	
 	}
 	render() {
 		return (
@@ -40,6 +40,10 @@ class Counter extends React.Component {
 		);
 	}
 }
+
+Counter.defaultProps = {
+	count: 0
+};
 
 ReactDOM.render(<Counter />, document.getElementById('app'));
 
